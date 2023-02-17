6902ebc402764ccfbd6b148b5e9fdc3af1e040f9
krishnadeep4 <68559994+keshu4@users.noreply.github.com>
2023-02-17 14:50:53 +0530

    css-file-added

--- src/component/pages/dashboard/index.js
+++ src/component/pages/dashboard/index.js
@@ -1,7 +1,8 @@
-import React, { useState } from 'react'
-import { Calendar } from 'react-calendar';
-import 'react-calendar/dist/Calendar.css';
-import moment from 'moment';
+import React, { useState } from "react";
+import { Calendar } from "react-calendar";
+import "react-calendar/dist/Calendar.css";
+import moment from "moment";
+import "./calender.css";
 
 export const Dashboard = () => {
   var dateToFormat = "2018-05-16 12:57:13"; //TIMESTAMP
@@ -9,12 +10,16 @@ export const Dashboard = () => {
 
   const leavedate = moment(dateToFormat).format("DD/MM/YYYY");
   const leavedate2 = moment(dateToFormat2).format("DD/MM/YYYY");
-  const [value, onChange] = useState(new Date);
-console.log(value)
+  const [value, onChange] = useState(new Date());
+  console.log(value);
   return (
-    <div>  
-      <Calendar onChange={onChange} value={value}  showWeekNumbers ={true} 
-      tileContent = {"rahul leave"}/>
+    <div>
+      <Calendar
+        onChange={onChange}
+        value={value}
+        showWeekNumbers={true}
+        tileContent={"rahul leave"}
+      />
     </div>
   );
-}
+};
