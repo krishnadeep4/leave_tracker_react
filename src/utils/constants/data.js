df4ffba272fc072e59233107fe6c10ecda00176c
shaikhibadurr <shaikhibadurr@gmail.com>
2023-02-17 16:18:27 +0530

    added the data.js

---
+++ src/utils/constants/data.js
@@ -0,0 +1,42 @@
+export const leaveDetails = [
+    {
+        "id": 1,
+        "leave_type": 1,
+        "reason": "casual",
+        "start_date": "2023-02-18",
+        "end_date": "2023-02-20",
+        "leave_days": 3,
+        "user_status": 0,
+        "user_id": 2
+    },
+    {
+        "id": 1,
+        "leave_type": 1,
+        "reason": "casual",
+        "start_date": "2023-02-14",
+        "end_date": "2023-02-16",
+        "leave_days": 3,
+        "user_status": 0,
+        "user_id": 2
+    },
+    {
+        "id": 1,
+        "leave_type": 1,
+        "reason": "casual",
+        "start_date": "2023-02-10",
+        "end_date": "2023-02-11",
+        "leave_days": 3,
+        "user_status": 0,
+        "user_id": 2
+    },
+    {
+        "id": 1,
+        "leave_type": 1,
+        "reason": "casual",
+        "start_date": "2023-02-09",
+        "end_date": "2023-02-09",
+        "leave_days": 3,
+        "user_status": 0,
+        "user_id": 2
+    },
+];
